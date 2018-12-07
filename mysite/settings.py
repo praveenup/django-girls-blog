50a6a7590295fa6babb9a8371732d86e45c502dd
praveenup <prakum.633@gmail.com>
2018-12-07 20:58:29 +0530

    added quiz application

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog',
     'users',
+    'quiz',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('quiz/', include('quiz.urls')),
     path('', include('blog.urls')),
 ]
 
